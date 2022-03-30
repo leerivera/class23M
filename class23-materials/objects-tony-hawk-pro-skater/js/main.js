a62713627bd96f8470a775372a3409da3750e053
leerivera <leerivera80@gmail.com>
2022-03-29 21:12:16 -0400

    skate

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,18 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function MakeSkater(playerName, playerSpeed, playerAgility, playerStamina){
+    this.name = playerName
+    this.speed = playerSpeed
+    this.agility = playerAgility
+    this.stamina = playerStamina
+}
+this.jump = function(){
+    alert('jumped')
+}
+
+this.race =function(){
+    alert("fast")
+}
+
+this.alley = function(){
+    alert('alleyed')
+}
