090916a696f4e3d72a070c7e9db5dc6e9091eafd
leerivera <leerivera80@gmail.com>
2022-03-29 20:58:51 -0400

    obj

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,11 @@
 //Create a dog object that has four properties and three methods
+
+let dog = {}
+dog.name = "woof woof"
+dog.fur = "black"
+dog.food = "kibble"
+dog.bed = "sleep"
+
+dog.bark = function(){
+    console.log("wooff!")
+}
