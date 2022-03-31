cbb21b28e984bbaf4c79cd9329d9cfe321e7ccf0
leerivera <leerivera80@gmail.com>
2022-03-31 12:02:23 -0400

    rockpaper

--- class23-materials/turbo-review/js/main.js
+++ class23-materials/turbo-review/js/main.js
@@ -1,18 +1,30 @@
 // // *Variables*
-// // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
-let sent = "wtf you want?"
-if (sent.indexOf('?') > -1){
-    alert('stop asking questions')
-}
+// // // Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
+// let sent = "wtf you want?"
+// if (sent.indexOf('?') > -1){
+//     alert('stop asking questions')
+// }
 
-// //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
-// let multi = " jr. dev here are some words for you cul"
-let stuff = "I am a junior dev or a software engineer"
-alert(stuff.replaceAll('junior dev', "software enginner"))
+// // //Declare a variable, assign it a string of multiple words, replace every "jr. dev" with "software engineer", and print it to the console
+// // let multi = " jr. dev here are some words for you cul"
+// let stuff = "I am a junior dev or a software engineer"
+// alert(stuff.replaceAll('junior dev', "software enginner"))
 
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
+function rockPaperScissors(){
+      let choice = Math.random();
+      if(choice < .66){
+          console.log('paper')
+      } else if(choice < .33){
+          console.log('scissors')
+      } else{
+          console.log('rock')
+      }
+}
+
+rockPaperScissors()
 
 
 // *Conditionals*
