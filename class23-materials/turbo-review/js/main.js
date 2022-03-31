80b2d1654f135827701a29fee8d22ae764965edc
leerivera <leerivera80@gmail.com>
2022-03-31 12:18:54 -0400

    more paper

--- class23-materials/turbo-review/js/main.js
+++ class23-materials/turbo-review/js/main.js
@@ -16,19 +16,30 @@
 function rockPaperScissors(){
       let choice = Math.random();
       if(choice < .66){
-          console.log('paper')
+          return('paper')
       } else if(choice < .33){
-          console.log('scissors')
+          return('scissors')
       } else{
-          console.log('rock')
+          return('rock')
       }
 }
 
-rockPaperScissors()
+// rockPaperScissors()
 
 
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+function checkWin(pChoice){
+    let botChoice = rockPaperScissors()
+    if((pChoice === "rock" && botChoice === "scissors") || (pChoice === "scissors" && botChoice === "paper") || (pChoice === "paper" && botChoice === "rock")){
+        alert("you won!")
+    } else if(pChoice === botChoice){
+        alert('ne gange pas putain')
+    } else {
+        alert("tu as perdu")
+    }
+}
 
+checkWin("paper")
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
